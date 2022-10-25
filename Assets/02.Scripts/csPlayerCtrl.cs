1135a4baac128421320523d4009e1de4bab4a0df
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2022-10-25 18:54:00 +0900

    20221025 / 조성욱 / 캐릭터 이동 함수, 캐릭터 시야 회전함수, 마우스 ONOFF함수 구분 + 대략적으로나마 enum으로 PlayerState 종류 만듦. ps. 이후 업데이트 내에서 계속 상태를 변경, 스타트에서 PlayerAnimState 코루틴 실행해서 3인칭 캐릭터에 계속해서 애니메이션 동기화 진행할 예정 우선 3인칭 캐릭터에 아무 애니메이션 작동하는 것 확인하기

--- Assets/02.Scripts/csPlayerCtrl.cs
+++ Assets/02.Scripts/csPlayerCtrl.cs
@@ -31,7 +31,8 @@ public class csPlayerCtrl : MonoBehaviour
     private Quaternion m_CharacterTargetRot;
     private Quaternion m_CameraTargetRot;
     private Transform playerCamera;
-    PlayerState playerState;
+    //열거형 변수 선언
+    State playerState = State.IDLE;
 
     //3인칭 캐릭터 애니메이션 변수
     int playerNetAnim;
@@ -43,10 +44,14 @@ public class csPlayerCtrl : MonoBehaviour
     Vector3 currPos = Vector3.zero;
     Quaternion currRot = Quaternion.identity;
 
+
     public PhotonView pv;
 
     //상태변수
     [HideInInspector] public bool isDie = false;
+    private bool isSlow = false;
+    private bool isDown = false;
+
 
     void Awake()
     {
@@ -71,6 +76,57 @@ public class csPlayerCtrl : MonoBehaviour
             m_CharacterTargetRot = transform.localRotation;
             m_CameraTargetRot = playerCamera.localRotation;
         }
+        else
+        {
+            //플레이어 상태에 따라 애니메이션 동기화 코루틴 구현 예정
+            StartCoroutine(this.PlayerAnimState());
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        CharacterMove();
+        CharacterView();
+        MouseOnOff();
+
+        //캐릭터 State 변경하는 부분 함수로 구현 예정
+        CharacterState();
+    }
+
+    //플레이어 상태에 따라 애니메이션 동기화 코루틴
+    IEnumerator PlayerAnimState()
+    {
+        while (!isDie)
+        {
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    //캐릭터 State 변경하는 부분 함수로 구현 예정
+    void CharacterState()
+    {
+
+    }
+
+
+    //캐릭터 상태 열거형
+    public enum State
+    {
+        IDLE = 0,
+        SLOWWORK,
+        WORK,
+        RUN,
+        DOOROPEN,
+        DIE,
+        SWITCHONOFF,
+        ITEMPICKUP,
+        ITEMFIND,
+        CHAIR,
+        CABINET,
+        DOWN,
+        RECOVERY
     }
 
     //1인칭, 3인칭 캐릭터 활성화
@@ -91,68 +147,37 @@ public class csPlayerCtrl : MonoBehaviour
         yield return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    //캐릭터 시야 회전 함수
+    void CharacterView()
     {
-        //캐릭터 조작 함수
-        CharacterControl();
-
-
-        if(controller.velocity != Vector3.zero)
+        if (pv.isMine)
         {
-            if (!pv.isMine)
-                anim.SetFloat("Move", 0.5f);
+            float yRot = Input.GetAxis("Mouse X") * rotSpeed;
+            float xRot = Input.GetAxis("Mouse Y") * rotSpeed;
+
+            //오일러와 쿼터니언에 대한 개념 필요 *상하 회전*
+            m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+            // 좌우 회전
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, m_CharacterTargetRot,
+                smoothTime * Time.deltaTime);
+            // 상하 회전
+            playerCamera.localRotation = Quaternion.Slerp(playerCamera.localRotation, m_CameraTargetRot,
+                    smoothTime * Time.deltaTime);
         }
-        //캐릭터 상태
-        //CharacterState();
-
-
-        ////캐릭터 애니메이션 함수
-        //if (!pv.isMine)
-        //{
-        //    CharacterAnim();
-        //}
+        else
+        {
+            //원격 플레이어의 아바타를 수신받은 각도만큼 부드럽게 회전시키자
+            transform.rotation = Quaternion.Slerp(transform.rotation, currRot, Time.deltaTime * 3.0f);
+        }
     }
 
-    //캐릭터 상태 열거형
-    public enum PlayerState
+    // 마우스 OnOff 함수
+    void MouseOnOff()
     {
-        IDLE = 0,
-        DIE,
-        MOVE,
-        PICKUP,
-
-
-    }
-
-    //캐릭터 상태 셋팅
-    //void CharacterState()
-    //{
-    //    if (controller.velocity != Vector3.zero)
-    //    {
-    //        playerState = PlayerState.MOVE;
-    //        playerNetAnim = 0;
-    //        if(pv.isMine)
-    //        {
-    //            anim.SetFloat("Move", 0.5f);
-    //        }
-    //    }
-    //}
-
-    //3인칭 캐릭터만 애니메이션이 재생되기 때문에  playerNetAnim 값으르 기준으로 한다.
-    //void CharacterAnim()
-    //{
-    //    switch (playerNetAnim)
-    //    {
-    //        case 0:
-    //            anim.SetFloat("Move", Mathf.Abs(controller.velocity.z));
-    //            break;
-    //    }
-    //}
-    //캐릭터 조작 함수
-    void CharacterControl()
-    {
-        //네트워크상 자신일때만 캐릭터 조작
         if (pv.isMine)
         {
             //ESC입력으로 마우스 보이게 하기
@@ -161,27 +186,20 @@ public class csPlayerCtrl : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
             }
+        }
+    }
 
+    //캐릭터 이동 함수
+    void CharacterMove()
+    {
+        //네트워크상 자신일때만 캐릭터 조작
+        if (pv.isMine)
+        {
             #region 캐릭터 조작
             float hor = Input.GetAxis("Horizontal") * movSpeed;
             float ver = Input.GetAxis("Vertical") * movSpeed;
-            float yRot = Input.GetAxis("Mouse X") * rotSpeed;
-            float xRot = Input.GetAxis("Mouse Y") * rotSpeed;
-
 
             moveDirection = new Vector3(hor, 0, ver);
-
-            //오일러와 쿼터니언에 대한 개념 필요 *상하 회전*
-            m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
-            m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
-
-
-            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
-
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, m_CharacterTargetRot,
-                smoothTime * Time.deltaTime);
-            playerCamera.localRotation = Quaternion.Slerp(playerCamera.localRotation, m_CameraTargetRot,
-                    smoothTime * Time.deltaTime);
 
             // transform.TransformDirection 함수는 인자로 전달된 벡터를 
             // 월드좌표계 기준으로 변환하여 변환된 벡터를 반환해 준다.
@@ -197,16 +215,11 @@ public class csPlayerCtrl : MonoBehaviour
         }
         else
         {
-            //Debug.Log(currPos + "currPos위치값");
-            //Debug.Log(transform.position + "transPos위치값");
-
             //원격 플레이어의 아바타를 수신받은 위치까지 부드럽게 이동시키자
             transform.position = Vector3.Lerp(transform.position, currPos, Time.deltaTime * 3.0f);
-            //원격 플레이어의 아바타를 수신받은 각도만큼 부드럽게 회전시키자
-            transform.rotation = Quaternion.Slerp(transform.rotation, currRot, Time.deltaTime * 3.0f);
-
         }
     }
+
     //X값 회전에 제한을 주기 위한 함수
     Quaternion ClampRotationAroundXAxis(Quaternion q)
     {
@@ -224,7 +237,7 @@ public class csPlayerCtrl : MonoBehaviour
         return q;
     }
 
-    ////위치값과 회전값 전송
+    ////위치값,회전값,애니메이션값 전달
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         Debug.Log("탄다");
