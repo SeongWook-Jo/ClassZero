7e0f4c7bec4829fe5590671ab9fefa1e0ecdeeb6
SEK0324 <tjd1373@gmail.com>
2022-10-26 23:05:01 +0900

    Open씬의 SoundManager 안의 UiOpen(){} 부분을 LobbyManager의 lobbySet_Open() 에 연결. Lobby의 Set 버튼 클릭 시 음량조절 PopUp이 열립니다. 추후 InGame에서는 ESC버튼으로 오픈되게 할 예정(아직 구현 X)
    현재 오픈-로비-레디 씬, 각 씬의 기본 버튼, Start BGM연결 되어있습니다.
    
    Effect BGM에는 인게임 상의 귀신 소리등 추가되면 연결할 예정
    
    SoundManager에서 PlaySoundBtn 없앰.. 근데 InGame에서 ESC 연결할 거니까
    혹시 몰라서 변수 지우지 않고 주석만 해놓음
    
    (성욱님 의문)
    -아이템 파밍할 때 위치 힌트 어떻게 줄것인지
    은경: 캐릭터 시선에 따른 위치에 문구 띄우기(ex 사물함이 흔들거린다, 책상에 낙서가 있다)
    다만 글씨가 뜬 곳에 아이템은 있을 수도 없을 수도 있다.
    
    -다른 유저가 사망시 죽은 위치 어떻게 알릴 것인지, 사망한 유저의 아이템 파밍 상태를
    어떻게 전달할 것인지(다른 유저의 아이템 창을 볼 수 있게 한다던지)
    
    지금 깃 푸쉬가 안되서^^ 내일 다시 푸쉬해볼 예정

---
+++ UiAssets/2.Script/LobbyManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{ 
+    public GameObject lobbyInfo_; // 게임설명
+    public GameObject lobbyRoom_; // 방 목록
+    public GameObject lobbyCredit_;
+
+    public GameObject[] lobbyBtn;  // 로비 화면 버튼들 참조 배열 
+
+    public void lobbySet_Open()
+    {
+        //lobbyBtn[0].SetActive(false); // 1.버튼 누르면 로비에 있는 버튼 비활성화시킴
+        //lobbyBtn[1].SetActive(false);
+        //lobbyBtn[2].SetActive(false);
+        //lobbyBtn[3].SetActive(false);
+        //lobbyBtn[4].SetActive(false);
+        //lobbyBtn[5].SetActive(false);
+
+        //lobbySet_.SetActive(true);     // 2. 패널 활성화
+        SoundManager soundmanager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        soundmanager.SoundUiOpen();
+
+
+    }
+
+    //public void OnClickSet()
+    //{
+    //    GameObject.Find("SoundManager");
+    //}
+
+    public void lobbyInfo_Open()
+    {
+        lobbyBtn[0].SetActive(false);
+        lobbyBtn[1].SetActive(false);
+        lobbyBtn[2].SetActive(false);
+        lobbyBtn[3].SetActive(false);
+        lobbyBtn[4].SetActive(false);
+        lobbyBtn[5].SetActive(false);
+
+        lobbyInfo_.SetActive(true);
+    }
+
+    public void lobbyInfo_Close()
+    {
+        lobbyInfo_.SetActive(false);
+
+        lobbyBtn[0].SetActive(true);
+        lobbyBtn[1].SetActive(true);
+        lobbyBtn[2].SetActive(true);
+        lobbyBtn[3].SetActive(true);
+        lobbyBtn[4].SetActive(true);
+        lobbyBtn[5].SetActive(true);
+
+    }
+
+    public void lobbyRoom_Open()
+    {
+        lobbyBtn[0].SetActive(false);
+        lobbyBtn[1].SetActive(false);
+        lobbyBtn[2].SetActive(false);
+        lobbyBtn[3].SetActive(false);
+        lobbyBtn[4].SetActive(false);
+        lobbyBtn[5].SetActive(false);
+
+        lobbyRoom_.SetActive(true);
+    }
+
+    public void lobbyRoom_Close()
+    {
+        lobbyRoom_.SetActive(false);
+
+        lobbyBtn[0].SetActive(true);
+        lobbyBtn[1].SetActive(true);
+        lobbyBtn[2].SetActive(true);
+        lobbyBtn[3].SetActive(true);
+        lobbyBtn[4].SetActive(true);
+        lobbyBtn[5].SetActive(true);
+    }
+
+    public void lobbyCredit_Open()
+    {
+        lobbyBtn[0].SetActive(false);
+        lobbyBtn[1].SetActive(false);
+        lobbyBtn[2].SetActive(false);
+        lobbyBtn[3].SetActive(false);
+        lobbyBtn[4].SetActive(false);
+        lobbyBtn[5].SetActive(false);
+
+        lobbyCredit_.SetActive(true);
+    }
+
+    public void lobbyCredit_Close()
+    {
+        lobbyCredit_.SetActive(false);
+
+        lobbyBtn[0].SetActive(true);
+        lobbyBtn[1].SetActive(true);
+        lobbyBtn[2].SetActive(true);
+        lobbyBtn[3].SetActive(true);
+        lobbyBtn[4].SetActive(true);
+        lobbyBtn[5].SetActive(true);
+    }
+
+    // Room 선택 시 준비로 넘어감
+    public void PlayGame()
+    {
+        // "안의 캔버스 컴포넌트 비활성화"
+        //GameObject.Find("").GetComponent<Canvas>().enabled = false;
+
+        SceneManager.LoadScene("scNetReady");
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("scNetUi");
+    }
+}
