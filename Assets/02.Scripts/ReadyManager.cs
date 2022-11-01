6c7c0a62f34393570411f37e949886315ce77b37
SEK0324 <tjd1373@gmail.com>
2022-11-01 22:50:12 +0900

    2022-10-26
    Ready 씬에 배경 이미지 삭제하고 발 보일 Floor랑 캐릭터(모델+ 각자 다른 idle) 추가, 변경
    ㄴ컨트롤러는 ReadyWalk1 2 3 ,  애니메이션 클립(Clip 밑에 Ready 폴더 추가)
    ㄴ Anim+model 된거 프리팹으로 만들어놨음. (프리팹 폴더 밑의 Ui_girl 폴더 추가)
    방과후의 해지기 직전, 노을지는 시간을 표현하기 위해 sky 에셋 다운..(Texture에 Sky 폴더 추가)
    ㄴ Sky들이랑 Anim Clip들...아직 완벽히 픽스 아니라 뒀는데 픽스 되면 안쓰는거 삭제할 예정
    
    +Ui 오른쪽 위의 !에 seting 함수 연결해놨는데...그냥 정말 함수 똑같이 추가해서 가져만 왔는데 저렇게 해도 되나
    모르겟음 뭐 작동은 문제없이 됩니다만..?
    
    하이라키의 Girls 밑, Girl들 프리팹을 off 해놨다가 ready 누르면 하나씩 SetActive true 해줄 것임...
    ㄴ아직 네트워크 적으로 ready 어떻게 구현할지 생각중...
    
    추후 ready 후 방장이 GameStart 누르면 InGame으로 씬 전환되게 구현할 예정...

--- Assets/02.Scripts/ReadyManager.cs
+++ Assets/02.Scripts/ReadyManager.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class ReadyManager : MonoBehaviour
 {
+    private PhotonView pv;
+    private bool[] playerReady = new bool[3];
+    
 
     private void Awake()
     {
         PhotonNetwork.isMessageQueueRunning = true;
+        
+        pv = GetComponent<PhotonView>();
     }
+    private void Start()
+    {
+        Debug.Log(PhotonNetwork.player.ID + "번째");
+    }
+    
     // Lobby로 되돌아감
     public void ReturnLobby()
     {
@@ -21,5 +31,22 @@ public class ReadyManager : MonoBehaviour
         PhotonNetwork.LeaveRoom();
     }
 
-    
+    public void lobbySet_Open()
+    {
+        SoundManager soundmanager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        soundmanager.SoundUiOpen();
+
+    }
+
+    public void OnPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerConnected()" + other.NickName);
+
+        if(PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient" + PhotonNetwork.isMasterClient);
+            
+        }
+    }
+
 }
