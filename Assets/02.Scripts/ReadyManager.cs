a6c11f3f1649b259fdca26227a9d5d738374ccd4
SEK0324 <tjd1373@gmail.com>
2022-11-04 17:55:43 +0900

    11-04 성은경
    Network에서 Lobby - Ready - InGame 모두 연결.
    
    로비에서 방 만들고, 세명 다 ready 눌렀을 때 MasterClient(방장) 에게 GameStart Btn 뜸.
    
    GameStartBtn 눌렀을 때 셋 다 InGame으로 넘어가고, InGame 씬에서 Exit버튼 누르면 로비로 돌아가게 연결해놓음
    
    근데, 가끔 로비에서 룸 리스트에 전에 만들었던 방 목록이 보이거나 같은 방이 여러개 뜨는 오류 있음...
    내 추측으로는 MasterClient가 LeaveRoom() 하지 않고 그냥 종료해버리면 방 나갔다고 안남아서 잔존하는것 같음
    아닐수도 있음
    
    + set 버튼 눌렀을 때 soundSeting이 안뜸... SoundCanvas는 씬 넘어왔을 때 가져와 졌는데
     SetActive(true);가 안됨..
    
    backSound 스크립트 추가(예전 Ui수업 때 LevelManager 였던 것...)
    ㄴ사운드 클립 일단 아무거나 넣어놨음 (테스트)
    
    추후 InGame UI 제작, 인벤토리를 위한 DB 구축, 연결 할 예정
    + 폴더는 추가된 것 없이 스크립트 backSound 하나만 추가됨.
    +전에 푸쉬안해서 GameStart 버튼도 추가될 것...
    + 레디했을 때 캐릭터 발...On Off 되는거 해야함

--- Assets/02.Scripts/ReadyManager.cs
+++ Assets/02.Scripts/ReadyManager.cs
@@ -6,80 +6,108 @@ using UnityEngine.SceneManagement;
 public class ReadyManager : MonoBehaviour
 {
     private PhotonView pv;
-    public bool?[] playerReady = new bool?[3] { null, null, null };
-    private int[] playerNumber = new int[100];
+    
+    public bool?[] playerReady = new bool?[3] { null,null,null};  // Network room에 들어온 Player.ID 를 받을 배열(maxplayer가 3명까지라 size 3)
+    private int[] playerNumber = new int[100]; // Player가 room에 들어올 때 마다 1씩 찍히기 때문에(찍어본 결과 나갔다 들어오면 추가로 쌓이면서 찍힘) 여유있게 100으로 설정
 
-    private void Awake()
+    public GameObject StartBtn;
+    public GameObject ReadyBtn;
+
+
+    void Awake()
     {
-        PhotonNetwork.isMessageQueueRunning = true;
-        pv = GetComponent<PhotonView>();
+        PhotonNetwork.isMessageQueueRunning = true; // 보내는걸 멈춰놨던 메세지 다시 true 해줌
+        
+        pv = GetComponent<PhotonView>(); // PhotonView 사용을 위해 pv를 가져온다,,
+        PhotonNetwork.automaticallySyncScene=true;
     }
-    private void Start()
+    
+    void Start()
     {
         Debug.Log(playerReady.Length);
         {
             playerNumber[PhotonNetwork.player.ID] = 0;
             playerReady[0] = false;
         }
-    }
-    private void Update()
-    {
-        if((bool)playerReady[0] && (bool)playerReady[1] && (bool)playerReady[2])
-        {
-            Debug.Log("11");
-        }
-    }
-    // Lobby로 되돌아감
-    public void ReturnLobby()
-    {
-        //PhotonNetwork.player.ID
-        // "안의 캔버스 컴포넌트 비활성화"
-        //GameObject.Find("").GetComponent<Canvas>().enabled = false;
-        PhotonNetwork.LeaveRoom();
-        SceneManager.LoadScene("scNetLobby");
+
     }
 
-    public void OnPhotonPlayerConnected(PhotonPlayer tempPlayer)
+    private void Update() // MasterClient는 Update에 종속
     {
-        for (int i = 0; i < playerReady.Length; i++)
+        
+        Debug.Log(playerReady[0] +" " +playerReady[1] + " "+ playerReady[2]);
+
+        if ((bool)playerReady[0] && (bool)playerReady[1] && (bool)playerReady[2])  // 여기가... 아예 못받아옴
         {
-            if (playerReady[i] == null)
+            
+            StartBtn.SetActive(true);
+        }
+        else
+        {
+            StartBtn.SetActive(false);
+        }
+
+    }
+
+    // Lobby로 되돌아감
+    public void OnClickReturnLobby()
+    {
+        // "안의 캔버스 컴포넌트 비활성화"
+        //GameObject.Find("").GetComponent<Canvas>().enabled = false;
+
+        SceneManager.LoadScene("scNetLobby");
+
+        PhotonNetwork.LeaveRoom();
+    }
+
+   
+    public void OnClickSet()  // set(sound)..
+    {
+        SoundManager soundmanager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        soundmanager.OnClickSoundUi_Close();
+        
+    }
+    
+    
+    public void OnPhotonPlayerConnected(PhotonPlayer other) //여기서 Player들 정보 받고, 참거짓 확인함.
+    {
+        Debug.Log("Connected");
+
+       for(int i=0; i<playerReady.Length; i++)
+        {
+            if(playerReady[i]==null)
             {
-                playerNumber[tempPlayer.ID] = i;
+                playerNumber[other.ID] = i;
                 playerReady[i] = false;
                 break;
             }
         }
     }
 
-    public void OnPhotonPlayerDisconnected(PhotonPlayer tempPlayer)
+    public void OnPhotonPlayerDisconnected(PhotonPlayer other)
     {
-        playerReady[playerNumber[tempPlayer.ID]] = null;
+        playerReady[playerNumber[other.ID]] = null;
     }
 
-    public void OnClickReady()
+    public void OnClickReady() // 버튼 누르면 MasterClient에게 player.ID 보내주고
     {
-        pv.RPC("Ready", PhotonTargets.All, PhotonNetwork.player.ID);
-    }
-    [PunRPC]
-    public void Ready(int id)
-    {
-        if (playerReady[playerNumber[id]] == false)
-            playerReady[playerNumber[id]] = true;
-        else
-            playerReady[playerNumber[id]] = false;
-    }
-    public void OnClickReturn()
-    {
-        pv.RPC("PlayerIdSend", PhotonTargets.MasterClient, PhotonNetwork.player.ID);
-    }
-    [PunRPC]
-    public void PlayerIdSend(int temp)
-    {
-        Debug.Log("플레이어 ID " + temp);
-        Debug.Log(playerReady[playerNumber[temp]]);
-        Debug.Log(playerReady[0] + " "+ playerReady[1] + " " + playerReady[2]);
+
+        pv.RPC("Ready", PhotonTargets.MasterClient,PhotonNetwork.player.ID);
     }
 
+    [PunRPC]
+    public void Ready(int other) // MasterClient가 player.ID 받아서 여기서 true/false 바꿔주는 작업을 한다.
+    {
+         playerReady[playerNumber[other]] = !playerReady[playerNumber[other]];
+    }
+
+
+    public void OnClickStartGame()  // 스타트 게임 버튼을 누르면 InGame 씬으로 전환
+    {
+        //PhotonNetwork.automaticallySyncScene; 어디?
+        PhotonNetwork.LoadLevel("scNetInGame");
+        
+        //SceneManager.LoadScene("scNetInGame");
+    }
 
 }
---
+++ Assets/02.Scripts/backSound.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backSound : MonoBehaviour
+{
+    //스테이지
+    public int stage;
+
+    //SoundManager컴포넌트를 연결할 변수 -> 인스펙터 뷰에는 꼭 필요한 변수만 노출시킴
+    private SoundManager _sMgr;
+
+    //테스트 변수
+    //public AudioClip soundClip;
+    //private float soundTime;
+
+    private void Awake()
+    {
+        //SoundManger 게임오브젝트의  SoundManger 컴포넌트 연결 
+        _sMgr = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  //로딩 완료후 설명서 비활성화 사운드 ui활성화
+        //  play매니져 끝에서 해줬던걸 씬 로드되서 해줄때는 밑에서처럼
+        // GameObject.Find ("ExPlainUi").gameObject.SetActive (false);
+        // GameObject.Find ("SoundCanvas").GetComponent<Canvas> ().enabled = true;
+
+        //배경 사운드 플레이
+        _sMgr.PlayBackground(stage);
+
+
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (Time.time > soundTime)
+    //    {
+    //        //3.5초마다 번개사운드 연출
+    //        LightningSound();
+
+    //        soundTime = Time.time + 3.5f;
+    //    }
+    //}
+
+    ////병렬 처리를 위한 코루틴 함수 호출
+    //void LightningSound()
+    //{
+    //    StartCoroutine(this.PlayEffctSound(soundClip)); //StartCoroutine으로 코루틴함수호출
+    //}
+
+    //Effect  테스트 사운드를 Coroutine으로 생성
+    //IEnumerator PlayEffctSound(AudioClip _clip)
+    //{
+    //    // 공용 사운드 함수 호출
+    //    _sMgr.PlayEffct(transform.position, _clip);
+    //    yield return null;
+    //}
+}
