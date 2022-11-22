e6cbb03515caf62d6e56ed87536a077575ec5135
SEK0324 <tjd1373@gmail.com>
2022-11-22 20:52:01 +0900

    03.Prefabs - Clue_Item에 악보, 낙서 프리팹 추가 및 ItemClass 폴더 추가 기존에 있던 ItemClss 만든 것 수정해서 이쪽에 옮겨둠
    07.Img에 악보 이미지 추가
    ㄴ악보 Mat 추가
    ㄴQuad 악보 prefab 추가(03.Prefab)
    ㄴ 단서 낙서 Qaud 추가 (Img 및 mat도 추가)
    
    02. Script
    => csInGameUiManager 스크립트에 tutorial popup 시켜주고 옆으로 넘겨주는 스크립트 추가

--- Assets/02.Scripts/UI/csInGameUiManager.cs
+++ Assets/02.Scripts/UI/csInGameUiManager.cs
@@ -18,6 +18,13 @@ public class csInGameUiManager : MonoBehaviour
     public GameObject Inven_Clue; //단서 프리팹, CluePrf 으로 태그 달았음
     public GameObject scrollContents; // 단서리스트랑 프리팹 생길 곳
 
+    // 튜토리얼
+    public GameObject tutorialBtn;
+    public Sprite[] tutorial_popup;   // 튜토리얼 이미지 4장
+    public Image tutorial;       // 하이라키의 Tutorial, 여기에 이미지 들어가야함
+    private int t_count=0;
+    
+
     // Map
     public GameObject M1;  //1층
     public GameObject M2;  //2층
@@ -30,13 +37,10 @@ public class csInGameUiManager : MonoBehaviour
     public Inventory theInventory;
     [SerializeField]
     private GameObject go_InventoryBase;  // 인벤토리 Page Onoff 시키기 위함... 부모로 하이라키에 만들어놓음
-    public bool inventoryActivated = false; // 인벤 켜지면 카메라나 다른 것 기능 안하게... 안움직이게 막는것
+    public static bool inventoryActivated = false; // 인벤 켜지면 카메라나 다른 것 기능 안하게... 안움직이게 막는것
     [SerializeField]
     private GameObject MainMenu;
-    public bool MainMenuActivated = false;
-    [SerializeField]
-    private GameObject Map;
-    public bool MapActivated = false;
+    public static bool MainMenuActivated = false;
 
     // Esc
     public void OnClickInfo()
@@ -44,100 +48,52 @@ public class csInGameUiManager : MonoBehaviour
         Info.SetActive(true);
     }
 
-    //public void OnCllick_SetBtn()//SetBtn - 사운드와 감도설정
-    //{       
-    //    SoundManager soundmanager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
-    //    soundmanager.SoundUiOpen();
 
-    //    //감도부분 추가예정
-
-    //}
-
-    //Inven
-
-    public bool InventoryOnOff()
+    public bool InventoryOn()
     {
+        inventoryActivated = !inventoryActivated;
         if (inventoryActivated)
         {
-            CloseInventory();
-            //CloseMainMenu();
-            //CloseMap();
-            return false;
+            OpenInventory();
+            CloseMainMenu();
+            return true;
         }
         else
         {
-            CloseMainMenu();
-            CloseMap();
-            OpenInventory();
-            return true;
+            CloseInventory();
+            return false;
         }
-        inventoryActivated = !inventoryActivated;
     }
-    public bool MainMenuOnOff()
+    public bool MainMenuOn()
     {
+        MainMenuActivated = !MainMenuActivated;
         if (MainMenuActivated)
         {
-            CloseMainMenu();
-            //CloseMainMenu();
-            //CloseMap();
-            return false;
-        }
-        else
-        {
             OpenMainMenu();
-            CloseMap();
             CloseInventory();
             return true;
-        }
-        MainMenuActivated = !MainMenuActivated;
-    }
-    public bool MapOnOff()
-    {
-        if (MapActivated)
-        {
-            CloseMap();
-            //CloseMainMenu();
-            //CloseMap();
-            return false;
         }
         else
         {
             CloseMainMenu();
-            OpenMap();
-            CloseInventory();
-            return true;
+            return false;
         }
-        MapActivated = !MapActivated;
     }
     private void OpenMainMenu()
     {
         MainMenu.SetActive(true);
-        MainMenuActivated = true;
     }
     private void CloseMainMenu()
     {
         MainMenu.SetActive(false);
-        MainMenuActivated = false;
     }
     private void OpenInventory()
     {
         go_InventoryBase.SetActive(true);
-        inventoryActivated = true;
     }
     private void CloseInventory()
     {
         go_InventoryBase.SetActive(false);
-        inventoryActivated = false;
-    }
-    private void OpenMap()
-    {
-        Map.SetActive(true);
-        MapActivated = true;
-    }
-    private void CloseMap()
-    {
-        Map.SetActive(false);
-        MapActivated = false;
     }
     public void OnClickInven_Item()
     {
@@ -153,28 +109,9 @@ public class csInGameUiManager : MonoBehaviour
 
         Inven_CluePage.SetActive(true); //  scroll view 가 popup 되고
 
-        // 그 영역에 instantiate(ClueList_prf)되고... ClueList_prf는 text 받아오고
-        //foreach ()
-        //{
-        //    GameObject clueList_prf = (GameObject)Instantiate(Inven_ClueList);
-
-        //    clueList_prf.transform.SetParent(scrollContents.transform, false);
-
-        //    Item itemtext = clueList_prf.text.GetComponent<Item>();
-        //    itemtext.itemtext = clueList_prf.text;
-        //}
-
-
+        
     }
 
-
-
-    //public void OnClick_ClueList() // 단서들중에 하나 골라서 읽으려고 클릭-> 단서 내용 읽을 수 있는 Page Popup
-    //{
-    //    Inven_ClueList.SetActive(false);
-    //    Inven_Clue.SetActive(true);
-
-    //}
 
     public void OnClick_X() //단서 PopUp된것 닫기
     {
@@ -189,6 +126,37 @@ public class csInGameUiManager : MonoBehaviour
         Inven_CluePage.SetActive(false);
 
         Inven_Special.SetActive(true);
+    }
+
+    public void OnClick_OpenTutorial()
+    {
+        tutorialBtn.SetActive(true);
+    }
+
+    public void OnClick_CloseTutorial()
+    {
+        tutorialBtn.SetActive(false);
+    }
+
+    public void OnClick_Tutorial_Right() // 단서에 기본으로 들어있는 튜토리얼 왼쪽으로 넘기기
+    {
+        t_count += 1;
+        
+        tutorial.sprite = tutorial_popup[t_count%4];
+
+    }
+
+    public void OnClick_Tutorial_Left() // 단서에 기본으로 들어있는 튜토리얼 오른쪽으로 넘기기
+
+    {
+        t_count -= 1;
+
+        if (t_count<0)
+        {
+            t_count = 3;
+        }
+
+        tutorial.sprite = tutorial_popup[t_count % 4];
     }
 
 
@@ -218,30 +186,19 @@ public class csInGameUiManager : MonoBehaviour
     //ReturnBtn
     public void OnClick_InGame_ReturnBtn() //InGame Ui에서 나와서 다시 게임으로 돌아감, ReturnBtn
     {
-        Map.SetActive(false);
-        MainMenu.SetActive(false);
-        go_InventoryBase.SetActive(false);
+        InGameUi.SetActive(false);
         MainMenuActivated = false;
         inventoryActivated = false;
-        MapActivated = false;
-        //구조상 문제로 무식하게 진행.. 리턴 버튼 클릭시 Player의 isStop false로 변경 및 커서보임.
-        GameObject myPlayer = GameObject.Find("Player_FP");
-        if(myPlayer != null)
-        {
-            myPlayer.transform.root.GetComponent<csPlayerCtrl>().isStop = false;
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
     }
 
     //ExitBtn 
     public void OnClickReturnLobby()// InGame에서 Lobby로 나감. LeaveRoom()- 네트워크 Room에서 나가고, LogMsg로 나간 PlayerId 
     {
-        // "안의 캔버스 컴포넌트 비활성화"
-        //GameObject.Find("").GetComponent<Canvas>().enabled = false;
+       
 
         SceneManager.LoadScene("scNetLobby");
-        PhotonNetwork.LeaveRoom();
+
+        
     }
 
 }
